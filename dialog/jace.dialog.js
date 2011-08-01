d1415f7701109c7f28642447a972b75ebda177ea
Ben Sparks <bsparks@unicon.net>
2011-08-01 20:03:15 +0000

    added start of widget file

---
+++ dialog/jace.dialog.js
@@ -0,0 +1,27 @@
+/*
+ * JACE Dialog
+ *
+ * Depends:
+ *   jquery.ui.core.js
+ *   jquery.ui.widget.js
+ */
+(function ($, undefined) {
+    $.widget("jace.dialog", {
+        
+        options: {
+            modal: false
+        },
+        
+        _create: function() {
+            var self = this;
+            
+            
+        },
+        
+        _destroy: function() {
+            
+        }
+        
+    });
+    
+}(jQuery));
